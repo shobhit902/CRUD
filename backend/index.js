a63b89e91a7401c7946a51a9e8c19bb458a28401
Shobhit Rawat <shobhit1948@gmail.com>
2025-08-03 22:24:42 +0530

    Created a backend folder

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,11 @@ import { connectDb } from "./db/index.js";
 dotenv.config();
 connectDb();
 
-
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send("hello")
-})
+app.get("/", (req, res) => {
+  res.send("hello");
+});
 
 app.listen(port, (req, res) => {
   console.log(`Listening at the port ${port}.`);
